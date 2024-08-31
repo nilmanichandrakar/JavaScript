ce8d0be32aefb643a47c66ff59e8baf478146236
Nilmani Chandrakar <46774689+nilmanichandrakar@users.noreply.github.com>
2024-08-31 19:10:13 +0000

    conversion of datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "nilmani"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+//"33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "neel"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 ==> true; 0 => false
+// "" => false
+// "neel" => true
+
+let someNumber =33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
